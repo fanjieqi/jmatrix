afffc5fda948b2dde51fa1924b1ac7893bc6ccdc
范捷琦 <jieqi.fan@corp.elong.com>
2016-09-15 01:07:43 +0800

    add the processing of @matrix_b

--- lib/matrix/matrix.rb
+++ lib/matrix/matrix.rb
@@ -5,19 +5,22 @@ class Matrix
     @matrix_b = matrix_b
     @rows = @matrix_a.count
     @cols = @matrix_a[0].count
+    @matrix_b.map!{|row| [row]} if !@matrix_b[0].is_a?(Array) && @rows > 1
   end
 
   def gaussian_elimination
-    matrix_a = @matrix_a.clone
+    matrix_a = @matrix_a.clone rescue nil
+    matrix_b = @matrix_b.clone rescue nil
     for k in (0 .. @rows - 1)
       for i in (k + 1 .. @rows - 1)
         times = matrix_a[i][k] / matrix_a[k][k].to_f
         for j in (k .. @cols - 1)
           matrix_a[i][j] = matrix_a[i][j] - times * matrix_a[k][j].to_f
+          matrix_b[i][j] = matrix_b[i][j] - times * matrix_b[k][j].to_f if j < matrix_b[i].count
         end
       end
     end
-    matrix_a
+    [matrix_a, matrix_b]
   end
 
 end
