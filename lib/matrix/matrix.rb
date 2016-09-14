00f9f9035ccf8881c6f89f39f6e798846e6d4b51
范捷琦 <jieqi.fan@corp.elong.com>
2016-09-15 00:58:56 +0800

    add the @rows and @cols to class:matrix

--- lib/matrix/matrix.rb
+++ lib/matrix/matrix.rb
@@ -3,6 +3,8 @@ class Matrix
   def initialize(matrix_a = nil, matrix_b = nil)
     @matrix_a = matrix_a
     @matrix_b = matrix_b
+    @rows = @matrix_a.count
+    @cols = @matrix_a[0].count
   end
 
 end
