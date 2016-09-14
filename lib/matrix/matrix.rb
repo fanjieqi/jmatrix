68f9e9b03ebcca6ec6dea18d82499c8028437ba4
范捷琦 <jieqi.fan@corp.elong.com>
2016-09-15 00:59:17 +0800

    add the matrix:gaussian_elimination

--- lib/matrix/matrix.rb
+++ lib/matrix/matrix.rb
@@ -7,4 +7,17 @@ class Matrix
     @cols = @matrix_a[0].count
   end
 
+  def gaussian_elimination
+    matrix_a = @matrix_a.clone
+    for k in (0 .. @rows - 1)
+      for i in (k + 1 .. @rows - 1)
+        times = matrix_a[i][k] / matrix_a[k][k].to_f
+        for j in (k .. @cols - 1)
+          matrix_a[i][j] = matrix_a[i][j] - times * matrix_a[k][j].to_f
+        end
+      end
+    end
+    matrix_a
+  end
+
 end
