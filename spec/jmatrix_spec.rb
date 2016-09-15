54719eb38a42f2ddfff774fc85d00d05329794f6
范捷琦 <jieqi.fan@corp.elong.com>
2016-09-15 20:25:28 +0800

    fix a bug in spec about the example params

--- spec/jmatrix_spec.rb
+++ spec/jmatrix_spec.rb
@@ -4,21 +4,22 @@ describe JMatrix do
 
   case1 = [[[1,2,3],[3,2,4],[4,5,6]], [6,7,8]]
   expected1 = [[[1, 2, 3], [0.0, -4.0, -5.0], [0.0, 0.0, -2.25]], [[6], [-11.0], [-7.75]]]
-
   describe ".gaussian_elimination" do
     context "given #{case1}" do
       it "returns #{expected1}" do
-        @matrix = JMatrix.new(case1[0], case1[1])
+        @matrix = JMatrix.new(case1[0].clone, case1[1].clone)
         expect(@matrix.gaussian_elimination).to eq expected1
       end
     end
   end
 
+  case2 = [[[1,2,3],[3,2,4],[4,5,6]], [6,7,8]]
+  expected2 = [[[1, 2, 3], [0.0, -4.0, -5.0], [0.0, 0.0, -2.25]], [[6], [-11.0], [-7.75]]]
   describe ".gaussian_elimination2" do
-    context "given #{case1}" do
-      it "returns #{expected1}" do
-        @matrix = JMatrix.new(case1[0], case1[1])
-        expect(@matrix.gaussian_elimination2).to eq expected1
+    context "given #{case2}" do
+      it "returns #{expected2}" do
+        @matrix = JMatrix.new(case2[0].clone, case2[1].clone)
+        expect(@matrix.gaussian_elimination2).to eq expected2
       end
     end
   end
