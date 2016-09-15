ce2b5b86b48e67404662283c2b9fe62a9bd22033
范捷琦 <jieqi.fan@corp.elong.com>
2016-09-15 17:19:57 +0800

    change the file naming and class name

--- lib/jmatrix.rb
+++ lib/jmatrix.rb
@@ -1,4 +1,4 @@
-class Matrix
+class JMatrix
 
   def initialize(matrix_a = nil, matrix_b = nil)
     @matrix_a = matrix_a
@@ -23,9 +23,29 @@ class Matrix
     [matrix_a, matrix_b]
   end
 
+  def gaussian_elimination2
+    matrix_a = @matrix_a.clone rescue nil
+    matrix_b = @matrix_b.clone rescue nil
+    matrix_a.each_with_index do |row, k|
+      matrix_a[k + 1 .. @rows - 1].each_with_index do |cur_row, i|
+        times = cur_row[k] / matrix_a[k][k].to_f
+        for j in (k .. @cols - 1)
+          cur_row[j] = cur_row[j] - times * matrix_a[k][j].to_f
+        end
+        matrix_b[i].each_with_index{|row, col| matrix_b[i][col] = matrix_b[i][col] - times * matrix_b[k][col]}
+      end
+    end
+    [matrix_a, matrix_b]
+  end
+
   def determinant
     matrix = gaussian_elimination[0]
     matrix.each.with_index.inject(1){|ans, (row,i)| ans *= row[i]}
   end
 
 end
+
+
+# p Matrix.new([[1,2,3],[3,2,4],[4,5,6]], [6,7,8]).gaussian_elimination
+# # p Matrix.new([[1,2,3],[3,2,4],[4,5,6]], [6,7,8]).gaussian_elimination2
+
