9f921644ae0e3223b6e6e9032c26511888f80a97
范捷琦 <jieqi.fan@corp.elong.com>
2016-09-15 18:40:00 +0800

    add the spec of gaussian_elimination2

--- spec/jmatrix_spec.rb
+++ spec/jmatrix_spec.rb
@@ -2,14 +2,23 @@ require "jmatrix"
 
 describe JMatrix do
 
-  describe ".gaussian_elimination" do
+  case1 = [[[1,2,3],[3,2,4],[4,5,6]], [6,7,8]]
+  expected = [[[1, 2, 3], [0.0, -4.0, -5.0], [0.0, 0.0, -2.25]], [[6], [-11.0], [-7.75]]]
 
-    case1 = [[[1,2,3],[3,2,4],[4,5,6]], [6,7,8]]
+  describe ".gaussian_elimination" do
     context "given #{case1}" do
-      expected = [[[1, 2, 3], [0.0, -4.0, -5.0], [0.0, 0.0, -2.25]], [[6], [-11.0], [-7.75]]]
       it "returns #{expected}" do
         @matrix = JMatrix.new(case1[0], case1[1])
         expect(@matrix.gaussian_elimination).to eq expected
+      end
+    end
+  end
+
+  describe ".gaussian_elimination2" do
+    context "given #{case1}" do
+      it "returns #{expected}" do
+        @matrix = JMatrix.new(case1[0], case1[1])
+        expect(@matrix.gaussian_elimination2).to eq expected
       end
     end
   end
