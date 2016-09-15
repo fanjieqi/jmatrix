bb060627f476e112984ff75c322efaea92f17a63
范捷琦 <jieqi.fan@corp.elong.com>
2016-09-15 22:48:11 +0800

    change the naming of case3 in spec

--- spec/jmatrix_spec.rb
+++ spec/jmatrix_spec.rb
@@ -13,13 +13,13 @@ describe JMatrix do
     end
   end
 
-  case3 = [[[1,2,3],[3,2,4],[4,5,6]], [[1,0,0],[0,1,0],[0,0,1]]]
-  expected3 = [[[1, 0.0, 0.0], [0.0, 1, 0.0], [0.0, 0.0, 1]], [[-0.8888888888888891, 0.33333333333333326, 0.2222222222222221], [-0.2222222222222222, -0.6666666666666666, 0.5555555555555556], [0.7777777777777778, 0.3333333333333333, -0.4444444444444444]]]
+  case2 = [[[1,2,3],[3,2,4],[4,5,6]], [[1,0,0],[0,1,0],[0,0,1]]]
+  expected2 = [[[1, 0.0, 0.0], [0.0, 1, 0.0], [0.0, 0.0, 1]], [[-0.8888888888888891, 0.33333333333333326, 0.2222222222222221], [-0.2222222222222222, -0.6666666666666666, 0.5555555555555556], [0.7777777777777778, 0.3333333333333333, -0.4444444444444444]]]
   describe ".gauss_jordan_elimination" do
-    context "given #{case3}" do
-      it "returns #{expected3}" do
-        @matrix = JMatrix.new(case3[0], case3[1])
-        expect(@matrix.gauss_jordan_elimination).to eq expected3
+    context "given #{case2}" do
+      it "returns #{expected2}" do
+        @matrix = JMatrix.new(case2[0], case2[1])
+        expect(@matrix.gauss_jordan_elimination).to eq expected2
       end
     end
   end
