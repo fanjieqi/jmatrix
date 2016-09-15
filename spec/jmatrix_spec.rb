42e1a895acac3872ac3af649fac3e39867c72cfc
范捷琦 <jieqi.fan@corp.elong.com>
2016-09-15 19:42:12 +0800

    change a naming in jmatrix_spec

--- spec/jmatrix_spec.rb
+++ spec/jmatrix_spec.rb
@@ -3,22 +3,22 @@ require "jmatrix"
 describe JMatrix do
 
   case1 = [[[1,2,3],[3,2,4],[4,5,6]], [6,7,8]]
-  expected = [[[1, 2, 3], [0.0, -4.0, -5.0], [0.0, 0.0, -2.25]], [[6], [-11.0], [-7.75]]]
+  expected1 = [[[1, 2, 3], [0.0, -4.0, -5.0], [0.0, 0.0, -2.25]], [[6], [-11.0], [-7.75]]]
 
   describe ".gaussian_elimination" do
     context "given #{case1}" do
-      it "returns #{expected}" do
+      it "returns #{expected1}" do
         @matrix = JMatrix.new(case1[0], case1[1])
-        expect(@matrix.gaussian_elimination).to eq expected
+        expect(@matrix.gaussian_elimination).to eq expected1
       end
     end
   end
 
   describe ".gaussian_elimination2" do
     context "given #{case1}" do
-      it "returns #{expected}" do
+      it "returns #{expected1}" do
         @matrix = JMatrix.new(case1[0], case1[1])
-        expect(@matrix.gaussian_elimination2).to eq expected
+        expect(@matrix.gaussian_elimination2).to eq expected1
       end
     end
   end
