483748ea2c0f3ad6d40348994e18efb1d723b482
范捷琦 <jieqi.fan@corp.elong.com>
2016-09-16 16:29:46 +0800

    simplify the code by using times_loop

--- lib/jmatrix/helper.rb
+++ lib/jmatrix/helper.rb
@@ -4,10 +4,9 @@ module JMatrixHelper
     rows = matrix_a.count
     cols = matrix_a[0].count
     matrix_c = Array.new(rows){ Array.new(cols, 0) }
-    (0..matrix_b.count - 1).each do |k|
-      (0..matrix_a.count - 1).each do |i|
-        (0..matrix_b[0].count - 1).each do |j|
-          matrix_c[i][j] ||= 0
+    matrix_b.count.times do |k|
+      matrix_a.count.times do |i|
+        matrix_b[0].count.times do |j|
           matrix_c[i][j] += matrix_a[i][k] * matrix_b[k][j]
         end
       end
