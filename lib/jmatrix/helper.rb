91c9dd5f021a355ef7315638e996061e268cb397
范捷琦 <jieqi.fan@corp.elong.com>
2016-09-16 15:17:57 +0800

    fix a naming bug

--- lib/jmatrix/helper.rb
+++ lib/jmatrix/helper.rb
@@ -1,18 +1,18 @@
 module JMatrixHelper
 
-  def multiply(martrix_a, martrix_b)
-    rows = martrix_a.count
-    cols = martrix_a[0].count
-    martrix_c = Array.new(rows){ Array.new(cols, 0) }
-    (0..martrix_b.count - 1).each do |k|
-      (0..martrix_a.count - 1).each do |i|
-        (0..martrix_b[0].count - 1).each do |j|
-          martrix_c[i][j] ||= 0
-          martrix_c[i][j] += martrix_a[i][k] * martrix_b[k][j]
+  def multiply(matrix_a, matrix_b)
+    rows = matrix_a.count
+    cols = matrix_a[0].count
+    matrix_c = Array.new(rows){ Array.new(cols, 0) }
+    (0..matrix_b.count - 1).each do |k|
+      (0..matrix_a.count - 1).each do |i|
+        (0..matrix_b[0].count - 1).each do |j|
+          matrix_c[i][j] ||= 0
+          matrix_c[i][j] += matrix_a[i][k] * matrix_b[k][j]
         end
       end
     end
-    martrix_c
+    matrix_c
   end
 
 end
